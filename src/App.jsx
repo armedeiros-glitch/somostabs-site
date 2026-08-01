1017e66c3257a20aef97000bd81e5ffedc122c00
armedeiros-glitch <armedeiros@live.com>
2026-07-31 22:44:59 -0300

    Conecta todas as seções na landing V2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import CinematicHero from './components/CinematicHero';
+import SiteSections from './components/SiteSections';
 
 export default function App() {
   return (
     <main>
       <CinematicHero />
+      <SiteSections />
     </main>
   );
 }
