b96e32181ff5908a86ba289dd9825e20838d98c7
armedeiros-glitch <armedeiros@live.com>
2026-09-04 10:44:35 -0300

    Add high quality ALT TABS product visuals

---
+++ hq-assets.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded',()=>{
+const p30='data:image/webp;base64,UklGRmYAAABXRUJQVlA4IFoAAADwFQCdASogAywAPpVEnEqlrriuKVIbKxASiWluwvf3V+NNkrMi7rT/X7xZP9uqAAAB0JaQAA==';
+});
